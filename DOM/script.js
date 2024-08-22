872b30564e2da431d1ee0d313a92db1d816bbc87
Hari <harikrishnan53828@gmail.com>
2024-08-23 02:16:21 +0530

    pondati nee

--- DOM/script.js
+++ DOM/script.js
@@ -9,5 +9,7 @@ document.getElementsByTagName("a")[0].innerHTML = "Hello World!!!!";
 document.getElementById('example').innerHTML = "<h1> New content</h1>"
 //setattribute
 
-document.getElementById('myImage').setAttribute("src", "js.jpg")
+document.getElementById('myImage').setAttribute("src", "js.jpg");
+
+document.getElementById("customElement").setAttribute("data-custom", "newValue");
     
