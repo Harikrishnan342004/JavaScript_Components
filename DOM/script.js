2b1fc8e37742cb009ebbb5e83074cd4e429ca399
Hari <harikrishnan53828@gmail.com>
2024-08-23 02:11:54 +0530

    heloo

--- DOM/script.js
+++ DOM/script.js
@@ -3,4 +3,11 @@ document.getElementsByClassName("remo")[0].innerHTML = "Vana";
 document.getElementsByClassName("remo")[1].innerHTML = "Vanakam da maplei";
 document.getElementsByTagName("a")[0].innerHTML = "Hello World!!!!";
 
+
+
+
+document.getElementById('example').innerHTML = "<h1> New content</h1>"
+//setattribute
+
+document.getElementById('myImage').setAttribute("src", "js.jpg")
     
