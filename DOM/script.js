222fe66316bc700bb519b007e575f5dd9ab10a90
Hari <harikrishnan53828@gmail.com>
2024-08-23 01:57:20 +0530

    Hari krishnan

---
+++ DOM/script.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").innerHTML = "Hello World!!!!";
+document.getElementsByClassName("remo")[0].innerHTML = "Vana";
+document.getElementsByClassName("remo")[1].innerHTML = "Vanakam da maplei";
+document.getElementsByTagName("a")[0].innerHTML = "Hello World!!!!";
+
+    
