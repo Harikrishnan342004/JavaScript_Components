c36613ba7f78d489622fdbf8186d23ca905d1ce1
Hari <harikrishnan53828@gmail.com>
2024-09-01 23:32:39 +0530

    callback

--- JavaScript_Callbacks/script.js
+++ JavaScript_Callbacks/script.js
@@ -1,16 +1,27 @@
-function myDislayer(some)
-{
-    document.getElementById("demo").innerHTML = some;
-}
-function myFirst()
-{
-    myDislayer("Hello");
-}
-function mySecond()
-{
-    myDislayer("Goodbye");
-}
+// function myDislayer(some)
+// {
+//     document.getElementById("demo").innerHTML = some;
+// }
+// function myFirst()
+// {
+//     myDislayer("Hello");
+// }
+// function mySecond()
+// {
+//     myDislayer("Goodbye");
+// }
 
 
-mySecond();
-myFirst();
+// mySecond();
+// myFirst();
+
+function myDislayer(something)
+{
+    document.getElementById("demo").innerHTML = something;
+}
+function myCalculator(num1 , num2 , myCallback)
+{
+    let sum = num1 + num2;
+    myCallback(sum);
+}
+myCalculator(5 ,  5 , myDislayer);
